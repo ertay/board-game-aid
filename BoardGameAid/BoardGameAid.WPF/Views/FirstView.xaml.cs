0b57862bce83d093836f5e243e6a6da9a87d1748
Ertay Shashko <ertay@outlook.com>
2018-03-08 23:52:05 +0100

    Add project files.

---
+++ BoardGameAid/BoardGameAid.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace BoardGameAid.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
